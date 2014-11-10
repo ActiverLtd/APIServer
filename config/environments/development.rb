fe38ecd8173359a5bb4d59a1fb388a55534014b1
RoopeHakulinen <roope_19xx@hotmail.com>
2014-11-10 19:12:28 +0200

    Debug log level.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,6 +29,8 @@ APIServer::Application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  config.log_level = :debug
+
   if "irb" == $0
 	  config.logger = Logger.new(Rails.root.join('log/console_output.txt'))
   end
