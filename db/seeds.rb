164aca5ddd18c951c8679fd30383db996d0f0623
Roope Hakulinen <roope_19xx@hotmail.com>
2015-03-27 05:49:29 +0200

    Added seeds for users that are invited or already matched.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,18 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 activityTypes = ActivityType.create([{name: "Sulkapallo"}, {name: "Tennis"}, {name: "Juoksu"}, {name: "Hiihto"}, {name: "Pingis"}, {name: "Suunnistus"}, {name: "Uinti"}])
 
-user = User.create({email: "roope.hakulinen@gmail.com", password: "password", authentication_token: "abcdefghijklmopqrstuvwxyz"})
-Activity.create(organizer: user, from: Time.now + 1.hour, to: Time.now + 2.hour, activity_type: activityTypes[0])
-Activity.create(organizer: user, from: Time.now + 1.day, to: Time.now + 2.day, activity_type: activityTypes[1])
-Activity.create(organizer: user, from: Time.now + 1.day, to: Time.now + 2.day, activity_type: activityTypes[2])
+roope = User.create({email: "roope.hakulinen@gmail.com", password: "password", authentication_token: "abcdefghijklmopqrstuvwxyz"})
+activity1 = Activity.create(organizer: roope, from: Time.now + 1.hour, to: Time.now + 2.hour, activity_type: activityTypes[0])
+activity2 = Activity.create(organizer: roope, from: Time.now + 1.day, to: Time.now + 2.day, activity_type: activityTypes[1])
+activity3 = Activity.create(organizer: roope, from: Time.now + 1.day, to: Time.now + 2.day, activity_type: activityTypes[2])
+
+user2 = User.create({email: "user2@example.com", password: "password", authentication_token: "abcdefghijklmopqrstuvwxyz2"})
+user3 = User.create({email: "user3@example.com", password: "password", authentication_token: "abcdefghijklmopqrstuvwxyz3"})
+user4 = User.create({email: "user4@example.com", password: "password", authentication_token: "abcdefghijklmopqrstuvwxyz4"})
+user5 = User.create({email: "user5@example.com", password: "password", authentication_token: "abcdefghijklmopqrstuvwxyz5"})
+user6 = User.create({email: "user6@example.com", password: "password", authentication_token: "abcdefghijklmopqrstuvwxyz6"})
+
+Suggestion.create(activity: activity1, user: user2, status: :match)
+Suggestion.create(activity: activity1, user: user3, status: :match)
+Suggestion.create(activity: activity2, user: user2, status: :invited)
+Suggestion.create(activity: activity2, user: user3, status: :invited)
