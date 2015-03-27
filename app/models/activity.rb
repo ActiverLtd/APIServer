c1445827e26b8a0c88912f51568bc78448b3caf7
Roope Hakulinen <roope_19xx@hotmail.com>
2015-03-27 05:53:48 +0200

    Fixed activity relations and rendering.

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,7 +2,7 @@ class Activity < ActiveRecord::Base
 	belongs_to :activity_type
 	belongs_to :organizer, class_name: 'User'
 	has_many :suggestions, dependent: :destroy
-	has_many :participants, -> { where suggestions: {status: 1} }, through: :suggestions, source: :user
+	has_many :participants, -> { where suggestions: {status: 2} }, through: :suggestions, source: :user
 	has_many :comments, dependent: :destroy
 
 	validates :activity_type_id, presence: true
